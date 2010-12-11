cb4305b1778b944c70e3aefe3752087b2d221810
oriong2@gmail.com <oriong2@gmail.com@96f2f15f-27fb-6d7c-70e3-fb0f206e73c4>
2010-12-11 14:36:18 +0000

    Synonyms class addded

---  ilproject --username oriong2/Classification/src/CategoryGenerator/CategoryGerenator.java
+++  ilproject --username oriong2/Classification/src/CategoryGenerator/CategoryGerenator.java
@@ -8,7 +8,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -26,83 +29,162 @@ import org.apache.lucene.util.Version;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 import dao.DAO_Model;
+import dominio.DbPedia;
+
 
 import Analizer.BlogSpaAnalyzer;
+import GetBlogText.WikipediaText;
 
 
 public class CategoryGerenator {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws CorruptIndexException 
-	 * @throws ParseException 
+	 * @throws Exception 
 	 */
 	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws CorruptIndexException, IOException, ParseException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+
+
+		Model ontology=DAO_Model.generateMainModel();
+		ArrayList<Resource> urlCategorias=getCategories(ontology);
+		System.out.println("Fin categorias");
 		
-		  
-		  Model ontology=DAO_Model.generateMainModel();
-		  
 		
+		Document categoria = new Document();
+		BlogSpaAnalyzer analyzer = new BlogSpaAnalyzer(Version.LUCENE_CURRENT);
+		Directory indexDirectory = FSDirectory.open(new File(".\\resources\\index"));
+		IndexWriter iwriter = new IndexWriter(indexDirectory, analyzer, true, new IndexWriter.MaxFieldLength(25000));
+		for(Resource resource: urlCategorias){
+			String text=getResourcesCategorias(resource.getURI());
+			String text2=getSubClasesCategorias(resource.getURI());
+
+			String text3= WikipediaText.GetTextFromWikipedia(resource.getLocalName());
+			Set<String> oSyn = Synonym.lookupSynonyms(resource.getLocalName());
+			 for(String sWord:oSyn){
+				 String sTextSynonym = WikipediaText.GetTextFromWikipedia(sWord);
+				 text3 += " " + sTextSynonym;
+			 }
+			
+			text.concat(text3).concat(text2);
+			
+			///ANALIZADORRRRRR
+
+			System.out.println("Contenido ficheros: "+ resource.getLocalName());
 		
+			//SE RECORREN TODOS LOS FICHEROS DE UNA CATEGORIA
+				//SE A�ADEN A CADA DOC=CATEGORIA
+			categoria.add( new Field("fieldname", text, Field.Store.YES,Field.Index.ANALYZED)); 
+			iwriter.addDocument(categoria);
+		}
+
+		iwriter.close();
 		
-		   BlogSpaAnalyzer analyzer = new BlogSpaAnalyzer(Version.LUCENE_CURRENT);
-		   
-		    // Store the index in memory:
-		    // Directory directory = new RAMDirectory();
-		    // To store an index on disk, use this instead:
-		    Directory indexDirectory = FSDirectory.open(new File(".\\resources\\index"));
-		 
-		   
-		    IndexWriter iwriter = new IndexWriter(indexDirectory, analyzer, true, new IndexWriter.MaxFieldLength(25000));
-		    
-		    //SE ABRE LA CARPETA
-		    File directorio=new File("./text_example");
-			String[] ListaCategorias=directorio.list();
-			  
-			//SE RECORRE CADA CATEGORIA=CARPETA
-		    for(int i=0;i<ListaCategorias.length;i++){
-			   ////1DOC POR CATEGORIA
-		      File carpeta=new File("./text_example/"+ListaCategorias[i]);
-		      String[] ListaFicheros=carpeta.list();
-		      System.out.println("Contenido ficheros: "+ ListaCategorias[i]);
-			  Document categoria = new Document();
-			  //SE RECORREN TODOS LOS FICHEROS DE UNA CATEGORIA
-		       for(int j=0;j<ListaFicheros.length;j++){
-		    	   FileReader file=null;
-			       System.out.println("Contenido ficheros: "+ ListaFicheros[j]);
-			       if(ListaFicheros[j].endsWith("htm")){	
-			    	    file= new FileReader("./text_example/"+ListaCategorias[i]+"/"+ListaFicheros[j]);
-			    	    BufferedReader reader= new BufferedReader(file);   
-			            String line = null;
-			            String text = "This is the text to be indexed.";
-			            try{
-				            while ((line = reader.readLine()) != null) {
-				                //Process the data, here we just print it out
-				            	  text=text+line;
-				            }
-			            }catch(Exception e){
-			            	System.out.print("error lectura");
-			            }
-					      
-					       //SE A�ADEN A CADA DOC=CATEGORIA
-			            
-			            categoria.add( new Field("fieldname", text, Field.Store.YES,Field.Index.ANALYZED)); 
-			       	}
-			       
-			       
-		        }
-		   
-		    iwriter.addDocument(categoria);
-		    }
-		 
-		    iwriter.close();
-		   		    
-		    
+
 	}
 
+	private static String getSubClasesCategorias(
+			String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static String getResourcesCategorias(
+			String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static ArrayList<Resource> getCategories(Model model) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
+		String rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns/#";
+		Property subClassOf = model.createProperty(rdfs,"subClassOf");
+		Property label = model.createProperty(rdfs,"label");
+		Property type=model.createProperty(rdf,"type");
+		//model.write(System.out);
+
+		// List everyone in the model who has a child:
+		ResIterator parents = model.listSubjectsWithProperty(subClassOf);
+		model.listObjects();
+		// Because subjects of statements are Resources, the method returned a ResIterator
+		System.out.println("Entra subjects categorias");
+		/*while (parents.hasNext()) {
+
+		  // ResIterator has a typed nextResource() method
+		  Resource object = parents.nextResource();
+
+		  // Print the URI of the resource
+		  System.out.println(object.getLocalName());
+		}*/
+
+		// Can also find all the parents by getting the objects of all "childOf" statements
+		// Objects of statements could be Resources or literals, so the Iterator returned
+		// contains RDFNodes
+		NodeIterator moreParents = model.listObjectsOfProperty(subClassOf);
+
+		// To find all the siblings of a specific person, the model itself can be queried 
+		//NodeIterator siblings = model.listObjectsOfProperty(edward, siblingOf);
+		System.out.println("\n\n\nEntra objects categorias");
+
+		//  po: http://purl.org/ontology/po/
+		//	http://www.w3.org/2002/07/owl# rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns/#
+		// skos http://www.w3.org/TR/skos-reference/skos.html# rdfs http://www.w3.org/2000/01/rdf-schema#
+		ArrayList<Resource> urlCategorias=new ArrayList<Resource>();
+		while (moreParents.hasNext()) {
+
+			// ResIterator has a typed nextResource() method
+			Resource object = (Resource) moreParents.nextNode();
+			System.out.println("\n\n"+object.getURI()+":\n\n");
+			urlCategorias.add(object);
+			if(!(object.getURI().equals("http://dbpedia.org/ontology/Eukaryote"))){
+				Model resourceModel=DbPedia.getRelatedModelFull(object.getURI());
+
+				//System.out.print(resourceModel.getRequiredProperty(object, label).getObject());
+				//System.out.println(object.getRequiredProperty(label).toString());
+				Resource res=null;
+
+				NodeIterator ni=resourceModel.listObjectsOfProperty(type);
+				//System.out.print(object.getURI());
+				while(ni.hasNext()){
+					RDFNode node=ni.next();
+					if(node.isResource()){
+						Resource r=(Resource)node;
+
+						System.out.print("/ Type = "+r.getLocalName());
+					}
+
+
+
+
+				}
+				NodeIterator ni1=resourceModel.listObjectsOfProperty(subClassOf);
+				//System.out.print(object.getURI());
+				while(ni1.hasNext()){
+					RDFNode node=ni1.next();
+
+					System.out.print("/ LABEL = "+node.toString());
+
+
+				}
+
+			}
+
+			// But it's more elegant to ask the Resource directly
+			// This method yields an iterator over Statements
+			//StmtIterator moreSiblings = edward.listProperties(siblingOf);
+
+
+
+		}
+		return urlCategorias;
+	}
 }
