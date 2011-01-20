7196f353d70f5534e60987c956d3b0020fd883ba
oriong2@gmail.com <oriong2@gmail.com@96f2f15f-27fb-6d7c-70e3-fb0f206e73c4>
2011-01-20 15:16:40 +0000

    Evaluator and score classes

---
+++ trunk/ ilproject --username oriong2/Classification/src/Classificator/Evaluator.java
@@ -0,0 +1,28 @@
+package Classificator;
+
+import java.util.HashMap;
+
+import DBLayer.DAOCategorization;
+
+
+
+public class Evaluator {
+
+
+public static int main(HashMap urlsCategorizadas){
+	double score=0;
+	double totalScore=0;
+	while(urlsCategorizadas.get(key)){
+		for(urls){
+			score=FinalScoreCalculator.getScoreWeb(url,cat);
+			totalScore=score+totalScore;
+		}
+		
+	}
+	System.out.print("Total score of evaluating "+totalScore);
+	
+	return 0;
+	
+	
+}
+}
---
+++ trunk/ ilproject --username oriong2/Classification/src/Classificator/FinalScoreCalculator.java
@@ -0,0 +1,19 @@
+package Classificator;
+
+public class FinalScoreCalculator {
+	private static double weightWiki= 0.2;	
+	private static double weightDbpedia= 0.2;	
+	private static double weightNews= 0.6;
+	
+	
+	public static void calculateFinalScoreTable(){
+		
+		
+		
+		double finalScore = score1*weighWiki+score2*weighDbpedia+
+		
+		
+		
+	}
+
+}
