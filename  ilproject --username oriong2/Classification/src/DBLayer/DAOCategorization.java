7155902b5962078d2433fa5b772e42cd19ecf3dc
oriong2@gmail.com <oriong2@gmail.com@96f2f15f-27fb-6d7c-70e3-fb0f206e73c4>
2011-01-21 11:28:43 +0000

    evaluator

---  ilproject --username oriong2/Classification/src/DBLayer/DAOCategorization.java
+++  ilproject --username oriong2/Classification/src/DBLayer/DAOCategorization.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 
+import CategoryGenerator.Categories;
+
 import dao.DAO_MySQL;
 
 
@@ -14,27 +16,27 @@ public class DAOCategorization extends DAO_MySQL{
 	
 
 	
-	public static ResultSet getEvaWeb(String login) throws SQLException {
+	public static ResultSet getEvaWeb(Categories category) throws SQLException {
 		// TODO Auto-generated method stub
 		Statement st = conexion.createStatement();
-	String query="SELECT * FROM eva_web WHERE name='"+login+"' ORDER BY value DESC";
+	String query="SELECT url,index,score FROM eva_web WHERE category='"+category+"' ORDER BY value url,index";
 		System.out.println(query);
 		ResultSet rs=st.executeQuery(query);
 
 		return rs;
 	}
 	
-	public static void storeWebCat(String login, String email, String pass) throws SQLException {
+	public static void storeWebCat(String url, String category, Float score) throws SQLException {
 		// TODO Auto-generated method stub
 		Statement st = conexion.createStatement();
-		st.executeUpdate("INSERT INTO web_cat(name,email,password) VALUES ('"+login+"','"+email+"','"+pass+"')");
+		st.executeUpdate("INSERT INTO web_cat(url,category,score) VALUES ('"+url+"','"+category+"','"+score+"')");
 		
 	}
 	
 	public static void storeEvaWeb(String domainUrl, int i, String stringValue,	float score) throws SQLException {
 		// TODO Auto-generated method stub
 		Statement st = conexion.createStatement();
-		st.executeUpdate("INSERT INTO eva_web(url,index,category,score) VALUES ('"+domainUrl+"','"+i+"','"+stringValue+"','"+score+"')");
+		st.executeUpdate("INSERT INTO web_cat_eva(url,index,category,score) VALUES ('"+domainUrl+"','"+i+"','"+stringValue+"','"+score+"')");
 		
 	}
 	
